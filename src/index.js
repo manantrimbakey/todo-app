28f8295f18c3d2c7abf1271527967dcb47d45040
manantrimbakey <manan.trimbakey@gmail.com>
2023-01-26 23:33:41 +0530

    First Commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "./salesforce-lightning-design-system-offline.min.css";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <div class="slds-p-horizontal_large slds-p-vertical_large full-height full-width">
+            <div class="slds-box_border slds-theme_default full-height-relative full-width-relative">
+                <div class=" slds-p-horizontal_large slds-p-vertical_large">
+                    <App />
+                </div>
+            </div>
+        </div>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
