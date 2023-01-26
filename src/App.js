4b953145a098b1bec4e90ada1c85197350eb8000
manantrimbakey <manan.trimbakey@gmail.com>
2023-01-27 00:22:06 +0530

    completed addtion and cancel button changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,58 @@
 import React from "react";
+import "./index.css";
+import NewNote from "./NewNote";
 
 export default class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isAddCardOn: false,
+        };
+    }
+
     render() {
-        return (
-            <div>
-                <button class="slds-button slds-button_brand">Hello</button>
+        let data = (
+            <div class="slds-card">
+                <div class="slds-card__header slds-grid">
+                    <div class="slds-media slds-media_center slds-has-flexi-truncate">
+                        <div class="slds-media__body">
+                            <h2 class="slds-text-heading_small">Notes App</h2>
+                        </div>
+                    </div>
+                    <div class="slds-no-flex">
+                        <div class="slds-button-group" role="group">
+                            <button
+                                class="slds-button slds-button_brand"
+                                onClick={() => {
+                                    this.handleAddClick();
+                                }}
+                            >
+                                Add
+                            </button>
+                            <button class="slds-button slds-button_neutral">Refresh</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="slds-card__body slds-p-horizontal_medium slds-p-vertical_medium">
+                    {this.state.isAddCardOn ? (
+                        <NewNote
+                            onClose={() => {
+                                this.onClose();
+                            }}
+                        />
+                    ) : null}
+                </div>
             </div>
         );
+        return data;
+    }
+
+    handleAddClick() {
+        console.log("Method Called");
+        this.setState({ isAddCardOn: true });
+    }
+
+    onClose() {
+        this.setState({ isAddCardOn: false });
     }
 }
---
+++ src/NewNote.js
@@ -0,0 +1,60 @@
+import React from "react";
+import "./index.css";
+
+export default class NewNote extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div class="slds-card">
+                <div class="slds-card__header slds-grid">
+                    <div class="slds-media slds-media_center slds-has-flexi-truncate">
+                        <div class="slds-media__body">
+                            <h2 class="slds-text-heading_small">Create New Note</h2>
+                        </div>
+                    </div>
+                    <div class="slds-no-flex">
+                        <div class="slds-button-group" role="group">
+                            <button class="slds-button slds-button_brand">Save</button>
+                            <button
+                                class="slds-button slds-button_destructive"
+                                onClick={() => {
+                                    this.handleClose();
+                                }}
+                            >
+                                Cancel
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <div class="slds-card__body slds-p-horizontal_medium slds-p-vertical_medium">
+                    <div class="slds-form-element">
+                        <label class="slds-form-element__label" for="noteTitle">
+                            Title
+                            <abbr class="slds-required" title="Required">
+                                *
+                            </abbr>
+                        </label>
+                        <div class="slds-form-element__control">
+                            <input type="text" id="noteTitle" class="slds-input" placeholder="Enter Title" />
+                        </div>
+                    </div>
+                    <div class="slds-form-element">
+                        <label class="slds-form-element__label" for="rich-text-area">
+                            Rich Text Area
+                        </label>
+                        <div class="slds-form-element__control">
+                            <textarea id="rich-text-area" class="slds-textarea"></textarea>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
+    handleClose() {
+        this.props.onClose();
+    }
+}
